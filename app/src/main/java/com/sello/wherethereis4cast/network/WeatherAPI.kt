8c4a295d1148fe725f3389f134a9ad8898ad2a0b
SelloN <s.nyama92@gmail.com>
2024-07-23 13:02:14 +0200

    * Add obfuscation * Refactor libs * Upgrade libs and agp

--- app/src/main/java/com/sello/wherethereis4cast/network/WeatherAPI.kt
+++ app/src/main/java/com/sello/wherethereis4cast/network/WeatherAPI.kt
@@ -1,11 +1,13 @@
 package com.sello.wherethereis4cast.network
 
+import androidx.annotation.Keep
 import com.sello.wherethereis4cast.model.Weather
 import com.sello.wherethereis4cast.utils.Constants
 import retrofit2.http.GET
 import retrofit2.http.Query
 import javax.inject.Singleton
 
+@Keep
 @Singleton
 interface WeatherAPI {
     @GET(value = "data/2.5/forecast/daily")
