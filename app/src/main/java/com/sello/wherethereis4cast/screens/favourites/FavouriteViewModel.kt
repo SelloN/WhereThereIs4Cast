b4fad5c606a8a962877c35bf41a1e86ce43dd7f5
SelloN <s.nyama92@gmail.com>
2024-07-26 00:24:45 +0200

    Minor refactors

--- app/src/main/java/com/sello/wherethereis4cast/screens/favourites/FavouriteViewModel.kt
+++ app/src/main/java/com/sello/wherethereis4cast/screens/favourites/FavouriteViewModel.kt
@@ -22,13 +22,8 @@ class FavouriteViewModel @Inject constructor(private val repository: WeatherDbRe
         viewModelScope.launch {
             repository.getFavourites().distinctUntilChanged()
                 .collect { listOfFavourites ->
-                    if (listOfFavourites.isEmpty()) {
-                        Log.d("TAG", ": Empty favourites ")
-                        _favouriteList.value = listOfFavourites
-                    } else {
-                        _favouriteList.value = listOfFavourites
-                        Log.d("FAV", ":${favouriteList.value} ")
-                    }
+                    _favouriteList.value = listOfFavourites
+                    Log.d("Favourites", ":${favouriteList.value} ")
                 }
         }
     }
